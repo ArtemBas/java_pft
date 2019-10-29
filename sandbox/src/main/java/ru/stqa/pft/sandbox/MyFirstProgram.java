083bc6907e98e0dac26827f871edb814546ed0c9
ArtemBas <razons#rambler.ru>
2019-10-29 19:09:39 +0200

    изменения 29.10.2019

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,22 +3,31 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.print("Hello, world!");
+    String e = "world!"; // String  переменная для строки !
+    System.out.print("Hello," + e);
 
-      System.out.println(2+2);
-      System.out.println(2*2);
-      System.out.println(2/2);
-      System.out.println(2-2);
+    System.out.println(2 + 2);
+    System.out.println(2 * 2);
+    System.out.println(2 / 2);
+    System.out.println(2 - 2);
 
-      System.out.println(1.0/2);
-      System.out.println(1/2.0);
-      System.out.println(2.0/2);
-      System.out.println("2"+"2");// конкотенация единственная операция для строк.
+    System.out.println(1.0 / 2);
+    System.out.println(1 / 2.0);
+    System.out.println(2.0 / 2);
+    System.out.println("2" + "2");// конкотенация единственная операция для строк.
 
-      System.out.println("2"+2);
+    System.out.println("2" + 2);
 
-      System.out.println((2+2)*2);
+    System.out.println((2 + 2) * 2);
 
-      System.out.println("2+2="+(2+2));
+    System.out.println("2+2=" + (2 + 2));
+
+    int l = 24; // int переменная для целых чисел!
+    int s = l * l;
+    System.out.println("Площадь квадрата со стороной" + l + "=" + s);
+
+    double q = 8.0; // double  переменная для чисел с плавающей точкой .!
+    double w = q * q;
+    System.out.println("Площадь квадрата со стороной" + q + "=" + w);
   }
 }
