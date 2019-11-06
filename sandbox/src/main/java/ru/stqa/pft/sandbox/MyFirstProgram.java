a60fa0405eb1f800fbed688bfcadf8883120fbbd
ArtemBas <razons#rambler.ru>
2019-11-06 16:59:16 +0200

    Изучение функции // Ну наконецто!!

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,31 +3,27 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    String e = "world!"; // String  переменная для строки !
-    System.out.print("Hello," + e);
+      hello ("world");
+      hello ("Natali");
+      hello ("User");
 
-    System.out.println(2 + 2);
-    System.out.println(2 * 2);
-    System.out.println(2 / 2);
-    System.out.println(2 - 2);
+      double l = 5;
+      System.out.println(" Площадь квадрата со стороной " + l + " = " + area(l));
 
-    System.out.println(1.0 / 2);
-    System.out.println(1 / 2.0);
-    System.out.println(2.0 / 2);
-    System.out.println("2" + "2");// конкотенация единственная операция для строк.
-
-    System.out.println("2" + 2);
-
-    System.out.println((2 + 2) * 2);
-
-    System.out.println("2+2=" + (2 + 2));
-
-    int l = 24; // int переменная для целых чисел!
-    int s = l * l;
-    System.out.println("Площадь квадрата со стороной" + l + "=" + s);
-
-    double q = 8.0; // double  переменная для чисел с плавающей точкой .!
-    double w = q * q;
-    System.out.println("Площадь квадрата со стороной" + q + "=" + w);
+      double a = 4;
+      double b = 6;
+      System.out.println(" Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
   }
+    public static void hello(String somebody ){
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area (double len){
+       return len*len;
+    }
+
+    public static double area ( double a, double b){
+        return a * b;
+    }
 }
+
